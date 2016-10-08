1cbcecc617643eaaf2340e9f2f0bb19a181891c3
chawean <chawean@gmail.com>
2016-10-09 00:56:36 +0700

    Update Security

--- workspace/PBP3/src/main/java/baiwa/config/SecurityConfig.java
+++ workspace/PBP3/src/main/java/baiwa/config/SecurityConfig.java
@@ -4,11 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import baiwa.util.BaiwaConstants;
+import baiwa.util.CustomLogoutHandler;
  
  
 @EnableWebSecurity 
@@ -50,12 +52,59 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
    *    Only "ADMIN" role can access url under /admin/** 
    *    Only "USER" role can access url under /**
    */
+//  @Override
+//  protected void configure(HttpSecurity http) throws Exception {
+//    http.authorizeRequests().antMatchers("/").permitAll(); 
+//    http.authorizeRequests().antMatchers("/imgs/**").permitAll(); 
+//    http.authorizeRequests().antMatchers("/theme/**").permitAll(); 
+//    http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN");
+//    http.authorizeRequests().antMatchers("/**").hasRole("USER").and().formLogin();
+//  }
+//  
+  
+  
   @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().antMatchers("/").permitAll(); 
-    http.authorizeRequests().antMatchers("/imgs/**").permitAll(); 
-    http.authorizeRequests().antMatchers("/theme/**").permitAll(); 
-    http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN");
-    http.authorizeRequests().antMatchers("/**").hasRole("USER").and().formLogin();
-  }
+ 	protected void configure(HttpSecurity http) throws Exception {
+ 		http.authorizeRequests()
+ 			.antMatchers(
+ 				"/", "/index.jsp",
+ 				BaiwaConstants.LOGIN_URL,
+ 				"/resources/**",
+ 				"/tmb/**",
+ 				"/rest/**"
+ 			).permitAll()
+ 			//.antMatchers("/admin/**").hasRole("ADMIN")
+ 			.anyRequest().hasAuthority(BaiwaConstants.ROLE_USER)
+ 		.and()
+ 		.formLogin()
+ 			.loginPage(BaiwaConstants.LOGIN_URL).permitAll()
+ 			.defaultSuccessUrl(BaiwaConstants.LOGIN_SUCCESS_URL)
+ 			//.successHandler(duplicateLoginAuthenticationSuccessHandler(BaiwaConstants.LOGIN_SUCCESS_URL, BaiwaConstants.LOGIN_WARNING_URL))
+ 			.failureUrl(BaiwaConstants.LOGIN_ERROR_URL).permitAll()
+ 			.usernameParameter("username")
+ 			.passwordParameter("password")
+ 		.and()
+ 		.logout()
+ 			.addLogoutHandler(customLogoutHandler())
+ 			.logoutUrl(BaiwaConstants.LOGOUT_URL)
+ 			.logoutSuccessUrl(BaiwaConstants.LOGIN_URL)
+ 			.invalidateHttpSession(true)
+ 			.clearAuthentication(true)
+ 		.and()
+ 		.exceptionHandling()
+ 			.accessDeniedPage("/403.htm")
+ 		.and()
+ 		.sessionManagement()
+ 			.maximumSessions(5)
+ 			//.sessionRegistry(sessionRegistry())
+ 			.expiredUrl(BaiwaConstants.LOGIN_URL);
+ 		
+ 		http.csrf().disable();
+ 	}
+   
+   
+ 	@Bean(name = "customLogoutHandler")
+ 	public CustomLogoutHandler customLogoutHandler() {
+ 		return new CustomLogoutHandler();
+ 	}
 }

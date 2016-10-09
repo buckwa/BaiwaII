4024d7a1e905c7c015924f17b0f8dd7e3166ace7
chawean <chawean@gmail.com>
2016-10-09 17:08:32 +0700

    Add theme resource

--- workspace/PBP3/src/main/java/baiwa/config/SecurityConfig.java
+++ workspace/PBP3/src/main/java/baiwa/config/SecurityConfig.java
@@ -70,6 +70,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
  				"/",
  				BaiwaConstants.LOGIN_URL,
  				"/resources/**",
+ 				"/theme/**",
  				"/tmb/**",
  				"/rest/**"
  			).permitAll()

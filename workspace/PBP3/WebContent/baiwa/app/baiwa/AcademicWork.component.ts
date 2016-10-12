f5be48d35c40ea9601b527cc6f92bf47e6dd3d33
keadtisak <keadtisak@xp-link.com>
2016-10-12 16:58:21 +0700

    add new file

---
+++ workspace/PBP3/WebContent/baiwa/app/baiwa/AcademicWork.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import {CommonService} from './../service/Common.service';
+
+
+@Component({
+    templateUrl: 'app/baiwa/html/AcademicWork.component.html'
+})
+export class AcademicWork {
+
+
+    constructor() {
+
+
+    }
+}

07d08b5c087f7d747c05587954ae50d97e6af09e
keadtisak <keadtisak@xp-link.com>
2016-10-12 22:17:22 +0700

    academy work

--- workspace/PBP3/WebContent/baiwa/app/baiwa/AcademicWork.component.js
+++ workspace/PBP3/WebContent/baiwa/app/baiwa/AcademicWork.component.js
@@ -9,14 +9,45 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var Common_service_1 = require('./../service/Common.service');
+var http_1 = require('@angular/http');
 var AcademicWork = (function () {
-    function AcademicWork() {
+    function AcademicWork(commonService, http) {
+        this.commonService = commonService;
+        this.http = http;
     }
+    AcademicWork.prototype.ngOnInit = function () {
+        this.GetUserSession();
+    };
+    AcademicWork.prototype.ngAfterViewInit = function () {
+    };
+    AcademicWork.prototype.GetUserSession = function () {
+        var _this = this;
+        var url = "../person/getUserSession";
+        return this.http.get(url).subscribe(function (response) { return _this.GetUserSessionSucess(response); }, function (error) { return _this.GetUserSessionError(error); }, function () { return console.log("editdoneUser !"); });
+    };
+    AcademicWork.prototype.GetUserSessionSucess = function (response) {
+        this.user = response.json(JSON.stringify(response._body));
+        this.GetAcademicWork(this.user.userName, this.user.currentAcademicYear, "1");
+    };
+    AcademicWork.prototype.GetUserSessionError = function (error) {
+        console.log("GetPersonError.");
+    };
+    AcademicWork.prototype.GetAcademicWork = function (user, year, round) {
+        var _this = this;
+        var url = "../person/getAcademicWork/" + user + "/" + year + "/" + round;
+        return this.http.get(url).subscribe(function (response) { return _this.GetUserAcademicSucess(response); }, function (error) { return _this.GetUserSessionError(error); }, function () { return console.log("editdoneUser !"); });
+    };
+    AcademicWork.prototype.GetUserAcademicSucess = function (response) {
+        this.academy = response.json(JSON.stringify(response._body));
+        this.academyList = this.academy.pBPWorkTypeList;
+        this.kpiuserList = this.academyList[0].academicKPIUserMappingList;
+    };
     AcademicWork = __decorate([
         core_1.Component({
             templateUrl: 'app/baiwa/html/AcademicWork.component.html'
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [Common_service_1.CommonService, http_1.Http])
     ], AcademicWork);
     return AcademicWork;
 }());
